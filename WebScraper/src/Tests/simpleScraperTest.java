4499195ab6034e399a2bb144c99df864aae4044f
jhrush21 <60750922+jhrush21@users.noreply.github.com>
2020-07-16 19:04:42 -0500

    Testing

--- WebScraper/src/Tests/simpleScraperTest.java
+++ WebScraper/src/Tests/simpleScraperTest.java
@@ -2,13 +2,33 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+
+
+import Classes.simpleScraper;
 
 public class simpleScraperTest {
 
 	@Test
-	public void test() {
-
+	public void csvChecktest() {
+		simpleScraper scrape = new simpleScraper();
+		File fileExpected = new File("csvFileExpected.txt");		
+		File fileActual = new File("csvFile.txt");
+		
+		try 
+		{
+			assertTrue("Difference detected!", FileUtils.contentEquals(fileExpected, fileActual));
+		} catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
