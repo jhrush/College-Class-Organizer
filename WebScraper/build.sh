e030dff5a8921e8f968d90f201854191fd98cf68
jhrush21 <jhrush@unomaha.edu>
2020-07-20 17:29:47 -0500

    Fixed shell scripts

--- WebScraper/build.sh
+++ WebScraper/build.sh
@@ -1,1 +1,1 @@
-javac -classpath src:lib/htmlunit-2.41.0-OSGi.jar "src/Classes/simpleScraper.java"
+javac -classpath src:lib/htmlunit-2.41.0-OSGi.jar "src/scraperDriver.java"
--- WebScraper/exec.sh
+++ WebScraper/exec.sh
@@ -1,1 +1,1 @@
-java -classpath src/:lib/htmlunit-2.41.0-OSGi.jar simpleScraper
+java -classpath src/:lib/htmlunit-2.41.0-OSGi.jar "scraperDriver"
