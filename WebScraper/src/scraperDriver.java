c3d1904105fb42a0dee86d8eccbe9f11880318b4
jhrush21 <jhrush@unomaha.edu>
2020-08-03 16:36:17 -0500

    Added cybersecurity to scraping!

--- WebScraper/src/scraperDriver.java
+++ WebScraper/src/scraperDriver.java
@@ -10,6 +10,8 @@ public class scraperDriver {
 		simpleScraper.getCourseLists("https://catalog.unomaha.edu/undergraduate/coursesaz/math/", "MATH.txt");
 		//CIST
 		simpleScraper.getCourseLists("https://catalog.unomaha.edu/undergraduate/coursesaz/cist/", "CIST.txt");
+		//CYBER
+		simpleScraper.getCourseLists("https://catalog.unomaha.edu/undergraduate/coursesaz/cybr/", "CYBER.txt");
 		//Core requirements
 		simpleScraper.getCoreReqs("https://catalog.unomaha.edu/undergraduate/college-information-science-technology/computer-science/computer-science-bs/#requirementstext");
 	}
