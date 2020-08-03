b157798dfa659530862559b15c12fbf0c6af471c
jhrush21 <jhrush@unomaha.edu>
2020-08-03 16:44:51 -0500

    Changed file name for CYBER

--- WebScraper/src/scraperDriver.java
+++ WebScraper/src/scraperDriver.java
@@ -11,7 +11,7 @@ public class scraperDriver {
 		//CIST
 		simpleScraper.getCourseLists("https://catalog.unomaha.edu/undergraduate/coursesaz/cist/", "CIST.txt");
 		//CYBER
-		simpleScraper.getCourseLists("https://catalog.unomaha.edu/undergraduate/coursesaz/cybr/", "CYBER.txt");
+		simpleScraper.getCourseLists("https://catalog.unomaha.edu/undergraduate/coursesaz/cybr/", "CYBR.txt");
 		//Core requirements
 		simpleScraper.getCoreReqs("https://catalog.unomaha.edu/undergraduate/college-information-science-technology/computer-science/computer-science-bs/#requirementstext");
 	}
