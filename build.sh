1c240108be0c1374abdfe2247366c6a6122b7f7c
jhrush21 <jhrush@unomaha.edu>
2020-08-13 15:04:45 -0500

    Added to bash scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
 javac -classpath WebScraper/src:WebScraper/lib/htmlunit-2.41.0-OSGi.jar "WebScraper/src/scraperDriver.java"
+FILE=CIST.txt
+if test -f "$FILE"; then
+rm CIST.txt
+rm CSCI.txt
+rm CYBR.txt
+rm MATH.txt
+rm REQS_1.txt
+rm REQS_2.txt
+rm REQS_3.txt
+fi
